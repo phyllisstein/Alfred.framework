6406a84e3201dea0e3aa7dd178eeaa36cceaa5a5
Daniel Shannon <d@daniel.sh>
2013-05-25 18:40:24 -0400

    Initial Commit

---
+++ Alfred/Alfred.h
@@ -0,0 +1,13 @@
+//
+//  Alfred.h
+//  Alfred
+//
+//  Created by Daniel Shannon on 5/25/13.
+//  Copyright (c) 2013 Daniel Shannon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Alfred : NSObject
+
+@end
